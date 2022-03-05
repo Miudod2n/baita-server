bb0e73a5b6846bfbfe743e1a68a16f1389a28175
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2022-03-04 21:44:00 -0300

    Update baitaserver.js

--- baitaserver.js
+++ baitaserver.js
@@ -29,5 +29,5 @@ baitaserver.get(':endpoint([\\/\\w\\.-]*)', function (req, res) {
 
 
 baitaserver.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port, baitaserver.settings.env);
 });
