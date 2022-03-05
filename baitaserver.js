c72ed32a95ac8496acc9addad1ff3d5801675cd6
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2022-03-04 21:15:18 -0300

    Add files via upload

---
+++ baitaserver.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from "cors"
+import axios from "axios"
+
+const baitaserver = express()
+baitaserver.use(cors());
+
+baitaserver.get(':endpoint([\\/\\w\\.-]*)', function (req, res) {
+    // Remove any trailing slash from base url
+
+    res.header("Access-Control-Allow-Origin", "*");
+
+    //var endpoint = req.header('Target-URL');
+    
+    
+    var endpoint = req.params.endpoint
+
+    endpoint = endpoint.substring(1) 
+    
+    axios.get(endpoint, {
+    }).then(async response => {
+        console.log(response.data)
+        res.json(await response.data) 
+           
+    }).catch(async error => {
+        res.json(await error)
+    })
+})
+
+
+baitaserver.listen(3000)
