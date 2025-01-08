5c69509778672a7d9d1acd62ad497118d2136993
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2025-01-08 18:21:07 -0300

    Update baitaserver.js

--- baitaserver.js
+++ baitaserver.js
@@ -5,8 +5,8 @@ import axios from "axios"
 const baitaserver = express()
 baitaserver.use(cors());
 
-baitaserver.get(':endpoint([\\w\\.-]*)', function (req, res) {
-//baitaserver.get(':endpoint([\\/\\w\\.-]*)', function (req, res) {
+//baitaserver.get(':endpoint([\\w\\.-]*)', function (req, res) => {
+baitaserver.get(':endpoint([\\/\\w\\.-]*)', function (req, res) => {
      // Remove any trailing slash from base url
 
     res.header("Access-Control-Allow-Origin", "*");
