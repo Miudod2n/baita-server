3d0b057bc4e717207a54c2dae106b5d039d48ab6
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2025-01-09 17:43:56 -0300

    Update baitaserver.js

--- baitaserver.js
+++ baitaserver.js
@@ -7,35 +7,30 @@ baitaserver.use(cors());
 
 const yada = "https://www.wikiaves.com.br";
 
-baitaserver.get(yada, function (req, res) {
-  var endpoint = req.header('Target-URL');
-  console.log(endpoint);
-  return res.json(await response.data) ;//res.redirect(301, req.path.slice(0, -1) + req.url.slice(req.path.length));
-  
-});
 
-//baitaserver.get(':endpoint([\\w\\.-]*)', function (req, res)  {
-     // Remove any trailing slash from base url
 
-    //res.header("Access-Control-Allow-Origin", "*");
+baitaserver.get(':endpoint([\\w\\.-]*)', function (req, res)  {
+    Remove any trailing slash from base url
 
-    //var endpoint = req.header('Target-URL');
+    res.header("Access-Control-Allow-Origin", "*");
+
+    var endpoint = req.header('Target-URL');
         
-    //var endpoint = req.params.endpoint;
-    //  var endpoint = req.path.substring(1);
-    //endpoint = endpoint.substring(1);
-   //        console.log(req.params.endpoint);
-   // console.log(endpoint);
-//axios.get(endpoint, {
+    var endpoint = req.params.endpoint;
+    var endpoint = req.path.substring(1);
+    endpoint = endpoint.substring(1);
+          console.log(req.params.endpoint);
+    console.log(endpoint);
+axios.get(endpoint, {
   
-  //  }).then(async response => {
+ }).then(async response => {
 
-   //     res.json(await response.data) 
+       res.json(await response.data) 
            
-   // }).catch(async error => {
-   //     res.json(await error)
-   // })
-//})
+    }).catch(async error => {
+        res.json(await error)
+   })
+  })
 
 
 baitaserver.listen(process.env.PORT || 3000, function(){
