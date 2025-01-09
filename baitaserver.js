307aa04ebd97303a672afaf67a63efcc29dcd9cc
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2025-01-09 16:21:59 -0300

    Update baitaserver.js

--- baitaserver.js
+++ baitaserver.js
@@ -5,28 +5,12 @@ import axios from "axios"
 const baitaserver = express()
 baitaserver.use(cors());
 
-baitaserver.use((req, res, next) => {
-  if (req.path.slice(-1) === '/' && req.path.length > 1) {
-    const query = req.url.slice(req.path.length)
-    const safepath = req.path.slice(0, -1).replace(/\/+/g, '/')
-      console.log("poop");
-   // res.redirect(301, safepath + query)
-      axios.get(safepath + query, {
+baitaserver.get('\\S+\/$', function (req, res) {
+  var endpoint = req.header('Target-URL');
+  console.log(endpoint);
+  return res.redirect(301, req.path.slice(0, -1) + req.url.slice(req.path.length));
   
-    }).then(async response => {
-
-        res.json(await response.data) 
-           
-    }).catch(async error => {
-        res.json(await error)
-    });
-      
-  } else {
-      console.log("pee");
-    next()
-  }
-})
-
+});
 
 //baitaserver.get(':endpoint([\\w\\.-]*)', function (req, res)  {
      // Remove any trailing slash from base url
@@ -40,11 +24,11 @@ baitaserver.use((req, res, next) => {
     //endpoint = endpoint.substring(1);
    //        console.log(req.params.endpoint);
    // console.log(endpoint);
-   // axios.get(endpoint, {
+//axios.get(endpoint, {
   
   //  }).then(async response => {
 
-    //    res.json(await response.data) 
+   //     res.json(await response.data) 
            
    // }).catch(async error => {
    //     res.json(await error)
