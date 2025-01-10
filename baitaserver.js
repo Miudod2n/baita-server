874797e551a4e41ecb132022419edaaabd6c4e7a
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2025-01-10 07:18:10 -0300

    Update baitaserver.js

--- baitaserver.js
+++ baitaserver.js
@@ -1,38 +1,30 @@
 import express from 'express'
 import cors from "cors"
 import axios from "axios"
+//import fetch from 'node-fetch';
+//import { HttpsProxyAgent } from 'https-proxy-agent';
 
 const baitaserver = express()
 baitaserver.use(cors());
 
-const yada = "https://www.wikiaves.com.br";
+baitaserver.get("/", function (req, res) {
+var yada = req.path.substring(1);
+    axios.get(yada, {}).then (async response => {res.json(await response.data)}).catch(async error => {res.json(await error)})})
+    
+
+//const yada = "https://www.wikiaves.com.br";
+//baitaserver.get(':endpoint([\\w\\.-]*)', function (req, res)  {
+////Remove any trailing slash from base url
+//res.header("Access-Control-Allow-Origin", "*");
+//var endpoint = req.header('Target-URL');
+//var endpoint = req.params.endpoint;
+//var endpoint = req.path.substring(1);
+//endpoint = endpoint.substring(1);
+//axios.get(endpoint, {}).then(async response => {
+//res.json(await response.data) 
+//}).catch(async error => {res.json(await error)})})
 
 
-
-baitaserver.get(':endpoint([\\w\\.-]*)', function (req, res)  {
-    //Remove any trailing slash from base url
-
-    res.header("Access-Control-Allow-Origin", "*");
-
-    var endpoint = req.header('Target-URL');
-        
-    var endpoint = req.params.endpoint;
-    var endpoint = req.path.substring(1);
-    endpoint = endpoint.substring(1);
-          console.log(req.params.endpoint);
-    console.log(endpoint);
-axios.get(endpoint, {
-  
- }).then(async response => {
-
-       res.json(await response.data) 
-           
-    }).catch(async error => {
-        res.json(await error)
-   })
-  })
-
-
-baitaserver.listen(process.env.PORT || 3000, function(){
+baitaserver.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, baitaserver.settings.env);
 });
