a6548a7b94629c07ea9ce083f5507819e69dff49
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2025-01-13 15:22:33 -0300

    Update baitaserver.js

--- baitaserver.js
+++ baitaserver.js
@@ -1,30 +1,49 @@
-import express from 'express'
-import cors from "cors"
-import axios from "axios"
-//import fetch from 'node-fetch';
-//import { HttpsProxyAgent } from 'https-proxy-agent';
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 
-const baitaserver = express()
-baitaserver.use(cors());
+// Grab the blacklist from the command-line so that we can update the blacklist without deploying
+// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
+// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
+// use originWhitelist instead.
+var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+function parseEnvList(env) {
+  if (!env) {
+    return [];
+  }
+  return env.split(',');
+}
 
-baitaserver.get("/", function (req, res) {
-var yada = req.path.substring(1);
-    axios.get(yada, {}).then (async response => {res.json(await response.data)}).catch(async error => {res.json(await error)})})
-    
+// Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
+var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
 
-//const yada = "https://www.wikiaves.com.br";
-//baitaserver.get(':endpoint([\\w\\.-]*)', function (req, res)  {
-////Remove any trailing slash from base url
-//res.header("Access-Control-Allow-Origin", "*");
-//var endpoint = req.header('Target-URL');
-//var endpoint = req.params.endpoint;
-//var endpoint = req.path.substring(1);
-//endpoint = endpoint.substring(1);
-//axios.get(endpoint, {}).then(async response => {
-//res.json(await response.data) 
-//}).catch(async error => {res.json(await error)})})
-
-
-baitaserver.listen(process.env.PORT || 3001, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, baitaserver.settings.env);
+var cors_proxy = require('./lib/cors-anywhere');
+cors_proxy.createServer({
+  originBlacklist: originBlacklist,
+  originWhitelist: originWhitelist,
+  requireHeader: ['origin', 'x-requested-with'],
+  checkRateLimit: checkRateLimit,
+  removeHeaders: [
+    'cookie',
+    'cookie2',
+    // Strip Heroku-specific headers
+    'x-request-start',
+    'x-request-id',
+    'via',
+    'connect-time',
+    'total-route-time',
+    // Other Heroku added debug headers
+    // 'x-forwarded-for',
+    // 'x-forwarded-proto',
+    // 'x-forwarded-port',
+  ],
+  redirectSameOrigin: true,
+  httpProxyOptions: {
+    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+    xfwd: false,
+  },
+}).listen(port, host, function() {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
